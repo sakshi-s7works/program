f63b0c6d02cdc79ce63fb694f14f8c9eece38e3a
Sakshi Agarwal <sakshi.agarwal52@gmail.com>
2024-02-14 10:44:50 +0530

    first commit

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", helloHandler)
+	port := 8080
+	fmt.Printf("Server is running on http://localhost:%d\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
